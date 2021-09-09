cafc29aa33d95d4139fbe8b997c64811ce525cfe
zotyev <mbixjkee1392@gmail.com>
2021-09-09 18:17:59 +0300

    Remove required error handler for model.

--- example/lib/impl/screen/test_page_model.dart
+++ example/lib/impl/screen/test_page_model.dart
@@ -6,7 +6,7 @@ class TestPageModel extends Model {
   int get value => _value;
   var _value = 0;
 
-  TestPageModel(ErrorHandler errorHandler) : super(errorHandler);
+  TestPageModel(ErrorHandler errorHandler) : super(errorHandler: errorHandler);
 
   Future<int> increment([int val = 0]) async {
     await Future<void>.delayed(const Duration(seconds: 1));
