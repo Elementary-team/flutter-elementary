87ecf681dc6eaafbf4ab9b5ab404f5b31adcc117
Михаил Зотьев <mbixjkee1392@gmail.com>
2021-10-21 00:15:05 +0300

    Update examples.

--- packages/elementary_test/example/test/unit/unit_helper.dart
+++
@@ -1,7 +0,0 @@
-import 'package:country/data/repository/country/country_repository.dart';
-import 'package:elementary/elementary.dart';
-import 'package:mocktail/mocktail.dart';
-
-class CountryRepositoryMock extends Mock implements CountryRepository {}
-
-class ErrorHandlerMock extends Mock implements ErrorHandler {}
