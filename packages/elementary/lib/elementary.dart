1dc6b451886fbbae4b87424f0ea3b3593d0f5d19
Михаил Зотьев <mbixjkee1392@gmail.com>
2021-10-21 00:29:07 +0300

    Add theme wrapper.

--- packages/elementary/lib/elementary.dart
+++ packages/elementary/lib/elementary.dart
@@ -5,3 +5,4 @@ export 'package:elementary/src/error/error_handler.dart';
 export 'package:elementary/src/relations/builder/state_notifier_builder.dart';
 export 'package:elementary/src/relations/notifier/state_notifier.dart';
 export 'package:elementary/src/ticker/ticker_providers.dart';
+export 'package:elementary/src/wrappers/theme_wrapper.dart';
