7180950a203b001fca299198c5445d70363eb522
Alexey Bukin <bukin.alexey.a@gmail.com>
2021-11-11 16:19:48 +0300

    added wm test generation

--- packages/elementary_tools/elementary_cli/lib/exit_code_exception.dart
+++ packages/elementary_tools/elementary_cli/lib/exit_code_exception.dart
@@ -15,14 +15,14 @@ class CommandLineUsageException extends IOException
   final String argumentName;
   final String argumentValue;
 
-  CommandLineUsageException({
-    this.message = "",
-    this.argumentName = "",
-    this.argumentValue = "",
-  });
-
   @override
   int get exitCode => 64;
+
+  CommandLineUsageException({
+    this.message = '',
+    this.argumentName = '',
+    this.argumentValue = '',
+  });
 
   @override
   String toString() {
@@ -42,40 +42,50 @@ class CommandLineUsageException extends IOException
 /// Exception thrown when user puts non-existent folder name.
 class NonExistentFolderException extends FileSystemException
     implements ExitCodeException {
-  NonExistentFolderException(String folderPath)
-      : super('Directory does not exist', folderPath);
-
   @override
   int get exitCode => 65;
+
+  NonExistentFolderException(String folderPath)
+      : super('Directory does not exist', folderPath);
 }
 
 /// Exception thrown when program cannot access template files.
 class GenerateTemplatesUnreachableException extends FileSystemException
     implements ExitCodeException {
-  GenerateTemplatesUnreachableException(
-      String message,
-      ) : super('Generator misses template files. Cause: ' + message);
-
   @override
   int get exitCode => 66;
+
+  GenerateTemplatesUnreachableException(
+    String message,
+  ) : super('Generator misses template files. Cause: $message');
 }
 
 /// Exception thrown when program tries to create existing file.
 class GeneratorTargetFileExistsException extends FileSystemException
     implements ExitCodeException {
-  GeneratorTargetFileExistsException(
-      String filepath,
-      ) : super('file already exists', filepath);
-
   @override
   int get exitCode => 67;
+
+  GeneratorTargetFileExistsException(
+    String filepath,
+  ) : super('file already exists', filepath);
 }
 
 /// Exception thrown when program cannot successfully generate files.
 class GenerationException extends FileSystemException
     implements ExitCodeException {
-  GenerationException() : super('generation cancelled: unknown error happened');
-
   @override
   int get exitCode => 68;
+
+  GenerationException() : super('generation cancelled: unknown error happened');
+}
+
+/// Exception thrown when user puts non-existent folder name.
+class NonExistentFileException extends FileSystemException
+    implements ExitCodeException {
+  @override
+  int get exitCode => 69;
+
+  NonExistentFileException(String filePath)
+      : super('File does not exist', filePath);
 }
