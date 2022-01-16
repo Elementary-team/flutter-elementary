2043aa06e17b3135bf2255619964d9ec96c31e14
Михаил Зотьев <mbixjkee1392@gmail.com>
2022-01-16 18:25:47 +0300

    Revert "[Elementary-test] Add params to test function (#39)" (#40)
    This reverts commit 454e21342aa9c5e8e68f26f869fbc966409456f7.

--- packages/elementary_test/lib/src/widget_model_test.dart
+++ packages/elementary_test/lib/src/widget_model_test.dart
@@ -14,42 +14,14 @@ import 'package:mocktail/mocktail.dart';
 /// [skip] - should skip the test. If passed a String or `true`, the test is
 /// skipped. If it's a String, it should explain why the test is skipped;
 /// this reason will be printed instead of running the test.
-///
-/// If [timeout] is passed, it's used to modify or replace the default timeout
-/// of 30 seconds. Timeout modifications take precedence in suite-group-test
-/// order, so [timeout] will also modify any timeouts set on the group or suite.
-///
-/// The description will be added to the descriptions of any surrounding
-/// [group]s. If [testOn] is passed, it's parsed as a [platform selector][]; the
-/// test will only be run on matching platforms.
-///
-/// If [tags] is passed, it declares user-defined tags that are applied to the
-/// test. These tags can be used to select or skip the test on the command line,
-/// or to do bulk test configuration. All tags should be declared in the
-/// [package configuration file][configuring tags]. The parameter can be an
-/// [Iterable] of tag names, or a [String] representing a single tag.
-///
-/// [onPlatform] allows tests to be configured on a platform-by-platform
-/// basis. It's a map from strings that are parsed as PlatformSelectors to
-/// annotation classes: [Timeout], [Skip], or lists of those. These
-/// annotations apply only on the given platforms.
-///
-/// If [retry] is passed, the test will be retried the provided number of times
-/// before being marked as a failure.
 @isTest
 void testWidgetModel<WM extends WidgetModel, W extends ElementaryWidget>(
   String description,
   WM Function() setupWm,
   dynamic Function(WM wm, WMTester<WM, W> tester, WMContext context)
       testFunction, {
-  String? testOn,
-  Timeout? timeout,
   // ignore: avoid_annotating_with_dynamic
   dynamic skip,
-  // ignore: avoid_annotating_with_dynamic
-  dynamic tags,
-  Map<String, dynamic>? onPlatform,
-  int? retry,
 }) {
   setUp(() {
     registerFallbackValue(WMContext());
@@ -62,12 +34,7 @@ void testWidgetModel<WM extends WidgetModel, W extends ElementaryWidget>(
       final element = _WMTestableElement<WM, W>(wm);
       await testFunction(wm, element, element);
     },
-    testOn: testOn,
-    timeout: timeout,
     skip: skip,
-    tags: tags,
-    onPlatform: onPlatform,
-    retry: retry,
   );
 }
 
