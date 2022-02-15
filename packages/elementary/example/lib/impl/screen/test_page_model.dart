1e1d26629c09acff4ba9fb12488a3a86594fcc37
Михаил Зотьев <mbixjkee1392@gmail.com>
2022-02-15 18:39:40 +0300

    Example: Simplifying the basic example.

--- packages/elementary/example/lib/impl/screen/test_page_model.dart
+++ packages/elementary/example/lib/impl/screen/test_page_model.dart
@@ -2,23 +2,18 @@ import 'dart:async';
 
 import 'package:elementary/elementary.dart';
 
+/// Model example. Business logic using
 class TestPageModel extends ElementaryModel {
   int get value => _value;
   var _value = 0;
 
   TestPageModel(ErrorHandler errorHandler) : super(errorHandler: errorHandler);
 
-  Future<int> increment([int val = 0]) async {
+  Future<int> increment() async {
+    // Emulate some long process like a request to server
     await Future<void>.delayed(const Duration(seconds: 1));
 
-    if (val > 0) {
-      _value += val;
-    } else {
-      ++_value;
-    }
-
-    // пример произошедшей ошибки, которую собираемся обработать визуально
-    handleError(Exception('Тестовая ошибка'));
+    ++_value;
 
     return _value;
   }
