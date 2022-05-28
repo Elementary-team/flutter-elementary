85a40156840230ed5cced9d2e35ed62f98496798
Михаил Зотьев <mbixjkee1392@gmail.com>
2022-05-28 14:27:36 +0400

    Feat: add 2022.1.1 IDE version support (#62)

--- packages/elementary_tools/plugin_intellij/build.gradle.kts
+++ packages/elementary_tools/plugin_intellij/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.3.1"
+    id("org.jetbrains.intellij") version "1.6.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.0"
+    id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.12"
 }
