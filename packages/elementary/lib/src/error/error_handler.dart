c29f112b9708d108818a5d0cf8bc91418645cc05
Fedor Blagodyr <bagotir@gmail.com>
2022-08-12 12:44:31 +0300

    chore(error-handler): edited docs
    Eddited documentation of handleError method

--- packages/elementary/lib/src/error/error_handler.dart
+++ packages/elementary/lib/src/error/error_handler.dart
@@ -3,6 +3,6 @@
 ///
 /// !!! This not for Presentation Layer handling.
 abstract class ErrorHandler {
-  /// This method have to handle of passed error.
+  /// This method have to handle of passed error and optional [StackTrace].
   void handleError(Object error, StackTrace? stackTrace);
 }
