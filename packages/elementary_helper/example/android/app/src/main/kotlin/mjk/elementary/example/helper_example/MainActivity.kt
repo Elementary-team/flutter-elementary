55a8b0ee402fbee856551ca010c9ee8dcb137c54
Mikhail Zotyev <mbixjkee1392@gmail.com>
2026-03-17 21:34:23 +0100

    Refactor: migrate Android example project configuration to Kotlin DSL and update dependencies.

--- packages/elementary_helper/example/android/app/src/main/kotlin/mjk/elementary/example/helper_example/MainActivity.kt
+++ packages/elementary_helper/example/android/app/src/main/kotlin/mjk/elementary/example/helper_example/MainActivity.kt
@@ -2,5 +2,4 @@ package mjk.elementary.example.helper_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
